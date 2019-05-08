3928146653a82be5da974c06cb88d8d61742ca21
Олександр Сорокін <o.sorokin@corp.arifru.dream.net.ua>
2019-05-08 18:52:00 +0300

    Add emit

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ let i = 0;
 
 server.on('request', (req, res) => {
     i++;
-    //res.end('Hello world.' ${i});
     res.end(`Hello world. ${i}`);
 });
 
-//const emit = server.emit;
+const emit = server.emit;
+server.emit = (...args) => {
 
-//server.emit//
+    console.log(args[0]);
+    return emit.apply(server, args);
+
+
+
+
+};
 
 server.listen(8000);
