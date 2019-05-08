20b14fd54b6d670ed3179d86f903fdfc0f052532
Олександр Сорокін <o.sorokin@corp.arifru.dream.net.ua>
2019-05-08 18:07:30 +0300

    All working

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const server = new http.Server();
 
 let i = 0;
 
-server.on('request',(req,res)=>{
+server.on('request', (req, res) => {
     i++;
-    res.end('Hello world. ${i}');
-
+    //res.end('Hello world.' ${i});
+    res.end(`Hello world. ${i}`);
 });
 
 //const emit = server.emit;
 
-//server.emit
+//server.emit//
 
 server.listen(8000);
