a7510d7ec42e99a1a67e0a588b7fb36868c91e5c
Олександр Сорокін <o.sorokin@corp.arifru.dream.net.ua>
2019-05-11 18:25:29 +0300

    added about modules commit

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ const handler = require('./handler');
 2. ./node_modules/handler
   ../node_modules/handler
   ../../node_modules/handler
-3. NODE_PATH=.
+3. NODE_PATH=. node server.js
+windows
+  set NODE_PATH=. && node server.js
+
 */
 
 server.on('request', handler);
-
+//var path = NODE_PATH=.;
 const emit = server.emit;
 server.emit = (...args) => {
-
+    //console.log(path);
     console.log(args[0]);
     return emit.apply(server, args);
-
-
-
 
 };
 
